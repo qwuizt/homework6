347ff32bf660f78b0c1009e99f73e779ab3a4da9
«qwoizt» <boringfine@gmail.com>
2022-08-29 03:49:05 +0300

    улучшенное задание 43

--- task43/Program.cs
+++ task43/Program.cs
@@ -9,11 +9,18 @@ int k2 = Convert.ToInt32(Console.ReadLine());
 
 double[] FindXY(double b1, double k1, double b2, double k2)
 {
-    double x = (b2 - b1) / (k1 - k2);
-    double y = k1 * x + b1;
     double[] array = new double[2];
-    array[0] = x;
-    array[1] = y;
+    if ((k1 == k2) || (b1 == 0 && k1 == 0 && b2 == 0 && k2 == 0))
+    {
+        Console.WriteLine("Прямые не пересекаются или лежат в (0;0).");
+    }
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        array[0] = x;
+        array[1] = y;
+    }
     return array;
 }
 
